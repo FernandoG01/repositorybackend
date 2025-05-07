9d7c78ee5fc557ea96f29a73cea9a398c86888c7
fernando guillen zegarra <fguillen@tecsup.edu.pe>
2025-05-07 16:57:22 -0500

    Agregamos package.json

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
-require('dotenv').config()
 const http= require('http')
 
 function requestController(){
-
+    console.log('Bienvenidos al curso')
 }
 
 const server=http.createServer(requestController)
 
-const PORT=process.env.PORT
-server.listen(PORT, function(){
-    console.log("Aplicaction corriente en: " + PORT)
-})
+server.listen(4000)
